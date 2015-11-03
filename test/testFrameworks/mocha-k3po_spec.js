1dcad3f0030dfeb9cfab49adef300788b4c72944
David Witherspoon <david.witherspoon@kaazing.com>
2015-11-02 17:47:54 -0800

    Added different ways of running tests

--- test/testFrameworks/mocha-k3po_spec.js
+++ test/testFrameworks/mocha-k3po_spec.js
@@ -4,7 +4,7 @@ var net = require('net');
 
 describe('TcpServer', function () {
 
-    setScriptRoot('org/kaazing/specification/control/assets')
+    setScriptRoot('org/kaazing/specification/control/assets');
 
     it('client', function () {
         var server = net.createServer(function(socket){
@@ -13,12 +13,12 @@ describe('TcpServer', function () {
                 socket.write('echo');
                 socket.pipe(socket);
                 socket.destroy();
-                k3poFinish(function(){
-                    server.close();
-                });
             });
         });
         server.listen(8000, '127.0.0.1', k3poStart);
+        k3poFinish(function(){
+            server.close();
+        });
     });
 
 
@@ -32,12 +32,29 @@ describe('TcpServer', function () {
                 socket.write('echo');
                 socket.pipe(socket);
                 socket.destroy();
-                k3poFinish(function(){
-                    server.close();
-                });
             });
         });
-        server.listen(8000, '127.0.0.1', k3poStart);
+        server.listen(8000, '127.0.0.1');
+        k3poFinish(function(){
+            server.close();
+        });
+    });
+
+    it('client.with.barriers', function () {
+        var server = net.createServer(function(socket){
+            awaitBarrier("NOTIFYING_BARRIER", function(){
+                notifyBarrier("AWAITING_BARRIER");
+            });
+            socket.on('data', function(data){
+                assert.equal("echo", data.toString());
+                socket.write('echo');
+                socket.pipe(socket);
+                socket.destroy();
+                server.close();
+            });
+        });
+        server.listen(8000, '127.0.0.1');
+        k3poFinish();
     });
 
 });
